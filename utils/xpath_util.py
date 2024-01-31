05f961fbe8de7f91e409d8072a9db32ff61f1527
林镕琛 <1311702391@qq.com>
2024-01-31 14:34:28 +0800

    update the file

--- utils/xpath_util.py
+++
@@ -1,9 +0,0 @@
-from selenium.webdriver.common.by import By
-
-
-def is_xpath_exist(bro, xpath):
-    try:
-        bro.find_element(By.XPATH, xpath)
-        return True
-    except:
-        return False
